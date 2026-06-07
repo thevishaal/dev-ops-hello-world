a790333743d603194680c7dc993c95378a8c60ec
thevishaal <thevishaal0@gmail.com>
2026-06-07 16:20:33 +0530

    fix: remove unnecessary code changes in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8000;
 
 app.get("/", (req, res) => {
-  return res.json({ message: "Server is healthy v3.1!", status "success" });
+  return res.json({ message: "Server is healthy v3.1!", status: "success" });
 });
 
 app.listen(PORT, () => {
